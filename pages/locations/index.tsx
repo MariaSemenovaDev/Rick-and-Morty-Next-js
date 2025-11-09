0e027a1050a2fecbf9250e916311432a8c3a88be
MariaSemenovaDev <semenovamk96@mail.ru>
2025-11-09 21:28:38 +0300

    second commit

---
+++ pages/locations/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {EpisodeType, LocationType, ResponseType} from "../../assets/api/rick-and-morty-api";
+import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
+import {Header} from "../../components/Header/Header";
+import {dehydrate, QueryClient, useQueries, useQuery} from "@tanstack/react-query";
+import {API} from "../../assets/api/api";
+import {Card} from "../../components/Card/Card";
+
+// export const getStaticProps = async () => {
+// }
+
+const getLocations = ( ) => {
+    return fetch ('https://rickandmortyapi.com/api/location', {
+        method: 'GET',
+    }).then((response) => response.json())
+}
+
+export const getStaticProps = async () => {
+
+    const queryClient = new QueryClient();
+
+    await queryClient.fetchQuery(['locations'], getLocations)
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        }
+    }
+
+}
+
+
+const Locations = () => {
+
+    const { data: locations } = useQuery<ResponseType<LocationType>>(['locations'], getLocations)
+
+    if(!locations) return null
+
+    const locationsList = locations.results.map(location => (
+        <Card key={location.id} name={location.name}/>
+    ))
+
+    return (
+        <PageWrapper>
+            <Header/>
+            {locationsList}
+        </PageWrapper>
+    );
+};
+
+export default Locations;
